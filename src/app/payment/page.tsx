1e7097a8aade86ae7c4e1c05cda57e67aa4af4a0
DenisNyux <denisn2000@gmail.com>
2023-10-31 18:11:28 +0100

    pre-prod-7

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -4,6 +4,7 @@ import getAllOtherDocuments from "@/api/documents/getAllOtherDocuments";
 import { DocumentCategories } from "@/types/DocumentsType";
 import removeExtension from "@/lib/removeExtension";
 
+
 async function Payment() {
   const documentsData: Promise<DocumentCategories> = getAllOtherDocuments();
   const documentCategories = await documentsData;
@@ -22,6 +23,7 @@ async function Payment() {
         <p>Из <a className="text-[#5e050d]" href={`${process.env.API_URL}/uploads/Ustav_RPA_5bb09f0cf5.pdf`}>устава</a> РПА</p>
         <i>Члены Организации обязаны уплачивать вступительный взнос, членские взносы, целевые и дополнительные взносы, установленные настоящим Уставом и решениями руководящих органов Организации;</i>
       </span>
+      
       <div className="grid grid-cols-2 gap-6 mt-6 md:grid-cols-1 ">
         {documents.map((document, index) => {
           return (
@@ -34,6 +36,9 @@ async function Payment() {
             ></RoundSquareLink>
           );
         })}
+      </div>
+      <div className="mt-6">
+      <RoundSquareLink url="https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=RPA&InvId=0&Culture=ru&Encoding=utf-8&OutSum=20&shp_interface=link&SignatureValue=939539440f267bc8fc6f5e390214a2e2" linkTitle="Оплатить членский взнос"></RoundSquareLink>
       </div>
     </div>
   );
